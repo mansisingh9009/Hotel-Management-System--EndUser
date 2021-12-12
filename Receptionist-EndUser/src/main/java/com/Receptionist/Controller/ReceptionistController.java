7edd48f08ef726137d8ce1ed90ba7262f3c79376
Mansi Singh <manasi.a.singh@capgemini.com>
2021-12-13 03:31:37 +0530

    comiting all changes

---
+++ Receptionist-EndUser/src/main/java/com/Receptionist/Controller/ReceptionistController.java
@@ -0,0 +1,15 @@
+package com.Receptionist.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Receptionist")
+public class ReceptionistController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Receptionist!!";
+	}
+}
